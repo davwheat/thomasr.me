7823fa6b2e5d8f24d4ce89d164d06d2a833d6fe2
Tom <git@tomr.me>
2021-12-01 04:13:32 +0000

    fix flickr pages (hopefully)

---
+++ assets/script/pages/flickr_sets.js
@@ -0,0 +1,48 @@
+---
+---
+function loaderBar() { return; }
+
+loaderBar(0);
+
+const outDiv = document.getElementById('js-gals');
+let outData = '';
+
+function doFlickr(flickrData) {
+  console.log(flickrData);
+  loaderBar(75);
+
+  for(album of flickrData.photosets.photoset) {
+    console.log(album);
+
+    let albumDate = new Date(parseInt(album.date_update) * 1000);
+    console.log(albumDate);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+    let desc = '';
+
+    if(album.description._content) {
+      desc = `
+        <br><span style="font-size: 0.8em; opacity: 0.6; font-style: italic;">${album.description._content}</span>
+      `;
+    }
+
+
+    outData += `
+      \n<li style="margin-bottom: 16px;">
+          <a href="https://www.flickr.com/photos/{{site.flickr}}/albums/${album.id}">
+          ${album.title._content}</a>
+          <br>
+          <span style="font-size: 0.8em; opacity: 0.8;">
+            ${albumDate.getDate()} ${months[albumDate.getMonth()]} ${albumDate.getFullYear()} &bull;
+            ${album.photos + album.videos} items &bull;
+            ${album.count_views} views
+          </span>
+          ${desc}
+        </li>
+    `;
+  }
+
+  outDiv.innerHTML = `<ul> ${outData} </ul>`;
+  loaderBar(100);
+
+}
