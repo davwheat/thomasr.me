4704217e9f7923d41c752a4e9fe2c356120e0248
Tom <git@tomr.me>
2021-12-08 05:14:30 +0000

    js for colours (#18)

--- assets/script/colours.js
+++ assets/script/colours.js
@@ -1,46 +1,60 @@
-// const colourNames = ['pink',    'bluel',   'blued',   'orange',  'green',   'red',     'purple',  'yellow'];
-// const colourCodes = ['#ff83a2', '#3dbbc6', '#3d76c6', '#c68d3d', '#3dc690', '#c6493d', '#663dc6', '#a9af26'];
+const colours = [
+     // tailwind colours       // 400   // 50    // 100   // 700or800  // 800 or 900
+    // {"name": "red", "colours": ["F87171","FEF2F2","FEE2E2","991B1B","7F1D1D"]}, // orange and red
+    // {"name": "ora", "colours": ["FB923C","FFF7ED","FFEDD5","9A3412","7C2D12"]}, // are too garish
+    {"name": "yel", "colours": ["FACC15","FEFCE8","FEF9C3","854D0E","713F12"]},
+    {"name": "eme", "colours": ["34D399","ECFDF5","D1FAE5","065F46","064E3B"]},
+    {"name": "cya", "colours": ["22D3EE","ECFEFF","CFFAFE","155E75","164E63"]},
+    {"name": "blu", "colours": ["60A5FA","EFF6FF","DBEAFE","1E40AF","1E3A8A"]},
+    {"name": "ind", "colours": ["818CF8","EEF2FF","E0E7FF","3730A3","312E81"]},
+    {"name": "pur", "colours": ["C084FC","FAF5FF","F3E8FF","6B21A8","581C87"]}
+    // {"name": "fuc", "colours": ["E879F9","FDF4FF","FAE8FF","86198F","701A75"]}
+    // {"name": "ros", "colours": ["FB7185","FFF1F2","FFE4E6","9F1239","881337"]} 
+];
 
-// if(localStorage.getItem('colours') !== 'off') {
-//     pickColour();
-// } else {
-//     document.getElementById('bw-button').href = 'javascript:enableColours()';
-//     document.getElementById('bw-button').innerHTML = '<i class="fas fa-fw fa-tint" aria-hidden="true"></i> go colourful';
-// }
+if(localStorage.getItem('colours') !== 'off') {
+    pickColour();
+} else {
+    document.getElementById('bw-button').href = 'javascript:enableColours()';
+    document.getElementById('bw-button').innerHTML = '<i class="fas fa-fw fa-tint" aria-hidden="true"></i> go colourful';
+}
 
-// function setColour(index) {
-//     let linkElement = document.createElement('link');
-//     linkElement.rel = 'stylesheet';
-//     linkElement.href = `/assets/style/colours/${colourNames[index]}.css`;
+function setCssAccent(a, c) {
+    document.querySelector(':root').style.setProperty(`--accent${a}`, `#${c}`);
+}
 
-//     document.head.appendChild(linkElement);
-//     document.querySelector('meta[name="theme-color"]').content = colourCodes[index];
+function setColour(c) {
+    document.querySelector('meta[name="theme-color"]').content = `#${c.colours[0]}`;
 
-//     localStorage.setItem('last-colour', colourNames[index]);
+    setCssAccent('', c.colours[0]);
+    setCssAccent('Light', c.colours[1]);
+    setCssAccent('LightIsh', c.colours[2]);
+    setCssAccent('DarkIsh', c.colours[3]);
+    setCssAccent('Dark', c.colours[4]);
 
-//     window.setTimeout(function(){document.getElementById('og-stylesheet').remove();},250);
-// }
+    localStorage.setItem('last-colour', c.name);
+}
 
-// function pickColour() {
-//     let randIndex = colourNames.length * Math.random() | 0;
+function pickColour() {
+    let randColour = colours[Math.floor(Math.random()*colours.length)];
 
-//     if(localStorage.getItem('last-colour') == colourNames[randIndex]) {
-//         pickColour();
-//     } else {
-//         return setColour(randIndex);
-//     }
-// }
+    if(localStorage.getItem('last-colour') == randColour.name) {
+        pickColour();
+    } else {
+        return setColour(randColour);
+    }
+}
 
-// function disableColours() {
-//     localStorage.setItem('colours', 'off');
-//     document.getElementById('bw-button').href = 'javascript:enableColours()';
-//     document.getElementById('bw-button').innerHTML = '<i class="fas fa-fw fa-tint" aria-hidden="true"></i> go colourful';
-//     window.location.reload();
-// }
+function disableColours() {
+    localStorage.setItem('colours', 'off');
+    document.getElementById('bw-button').href = 'javascript:enableColours()';
+    document.getElementById('bw-button').innerHTML = '<i class="fas fa-fw fa-tint" aria-hidden="true"></i> go colourful';
+    window.location.reload();
+}
 
-// function enableColours() {
-//     localStorage.setItem('colours', 'on');
-//     document.getElementById('bw-button').href = 'javascript:disableColours()';
-//     document.getElementById('bw-button').innerHTML = '<i class="fas fa-fw fa-tint-slash" aria-hidden="true"></i> go neutral';
-//     window.location.reload();
-// }
+function enableColours() {
+    localStorage.setItem('colours', 'on');
+    document.getElementById('bw-button').href = 'javascript:disableColours()';
+    document.getElementById('bw-button').innerHTML = '<i class="fas fa-fw fa-tint-slash" aria-hidden="true"></i> go neutral';
+    window.location.reload();
+}
